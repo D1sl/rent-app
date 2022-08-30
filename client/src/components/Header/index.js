269578073f719921452a3b97bded68dc5670b0d4
D1sl <94911312+D1sl@users.noreply.github.com>
2022-08-30 08:35:55 +0100

    updated header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,9 +1,34 @@
 import './style.css';
+import logo from '../../assets/img/logo.png';
+import magnifyingGlass from '../../assets/img/magnifying-glass.png';
+import emptyProfile from '../../assets/img/empty-profile.png';
 
 const Header = () => {
     return (
         <div className='main-header'>
-            <h1>Header</h1>
+            <div className='header-content'>
+                <div className='main-header-left'>
+                    <img src={logo} className="logo" alt="logo" />
+                </div>
+                <div className='main-header-center'>
+                    <nav>
+                        <ul>
+                            <li>Where</li>
+                            <li>Type of Home</li>
+                            <li>More</li>
+                        </ul>
+                        <div className="search-button">
+                            <img src={magnifyingGlass} className="search-icon" alt="magnifying glass" />
+                        </div>
+                    </nav>
+                </div>
+                <div className='main-header-right'>
+                    <div className="button login-button">
+                        <div className="button-text">Sign in</div>
+                        <img src={emptyProfile} className="empty-profile" alt="grey person" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
